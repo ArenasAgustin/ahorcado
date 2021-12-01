e6f57e91fe885c8fdaa52935d845d7e54786da23
“ArenasAgustin” <arenasagustin7@gmail.com>
2021-12-01 19:47:42 -0300

    personita

--- src/Dibujo/Dibujo.jsx
+++ src/Dibujo/Dibujo.jsx
@@ -5,11 +5,21 @@ export default function Dibujo() {
         <div className="contenedor">
             <div className="contenedor-persona">
                 <div className="persona-cabeza"></div>
-                <div className="persona-brazo derecho"></div>
-                <div className="persona-brazo izquierdo"></div>
-                <div className="persona-cuerpo"></div>
-                <div className="persona-pierna derecha"></div>
-                <div className="persona-pierna izquieda"></div>
+
+                <div className="contenedor-brazos">
+                    <div className="persona-brazo derecho"></div>
+
+                    <div className="persona-cuerpo"></div>
+
+                    <div className="persona-brazo izquierdo"></div>
+                </div>
+
+
+                <div className="contenedor-piernas">
+                    <div className="persona-pierna derecha"></div>
+
+                    <div className="persona-pierna izquieda"></div>
+                </div>
             </div>
         </div>
     )
