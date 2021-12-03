f026a6aaacb7a619b22c6bea38362e89f0693c49
Juani Solari <jignaciosolari@gmail.com>
2021-12-03 17:46:24 -0300

    cssLanding - globalState with levels

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import {getEasyLevel, getMediumLevel, getHardLevel} from '../../redux/actions/index'
 import './landing.scss'
 
 const Landing = () => {
+    
+    const dispatch = useDispatch()
+
+    const handleLevel = (level) => {
+        console.log('!!!!!!!!! HANDLE', level)
+        if(level === "EASY") return dispatch(getEasyLevel())
+        if(level === "MEDIUM") return dispatch(getMediumLevel())
+        return dispatch(getHardLevel())
+    }
+
     return (
        <div className="container">
            <h1>Ahorcado</h1>
@@ -10,11 +22,17 @@ const Landing = () => {
                 <h3>Hoy con que nivel te desafias?</h3>
                 <div className="container-btns">
                     <div className="btns">
-                         <Link to= 'home'>
-                            <button className="btn">Facil</button>
-                        </Link> 
-                            <button  className="btn">Medio</button>
-                            <button  className="btn">Dificil</button>
+                         <Link to= '/home' className="btn">
+                            <span className="btn" onClick={e => handleLevel("EASY")}>Facil</span>
+                        </Link>
+
+                        <Link to= '/home' className="btn"> 
+                            <span className="btn" onClick={e => handleLevel("MEDIUM")}>Medio</span>
+                        </Link>
+
+                        <Link to= '/home' className="btn">
+                            <span className="btn" onClick={e => handleLevel("HARD")}>Dificil</span>
+                        </Link>    
                     </div>
                 </div>
            </div>
