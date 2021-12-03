77e29e9a7070533bb8b82823e4c30b031140d2a2
Juani Solari <jignaciosolari@gmail.com>
2021-12-03 17:46:03 -0300

    cssLanding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import Dibujo from "./Components/Dibujo/Dibujo";
 import Landing from "./Components/Landing/Landing";
+import Home from "./Components/Home/Home.jsx"
+import { Route, Routes } from "react-router-dom"
+
 
 function App() {
   return (
     <div className="App">
-      <Landing />
-      <Home />
+    <Routes>
+      <Route exact path = "/" element={<Landing />} />
+      <Route exact path = "/home" element={<Home/>} /> 
+    </Routes>
     </div>
   );
 }
--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './landing.scss'
 
 const Landing = () => {
     return (
        <div className="container">
+           <h1>Ahorcado</h1>
            <div className="containerlevels">
-               
+                <h3>Hoy con que nivel te desafias?</h3>
+                <div className="container-btns">
+                    <div className="btns">
+                         <Link to= 'home'>
+                            <button className="btn">Facil</button>
+                        </Link> 
+                            <button  className="btn">Medio</button>
+                            <button  className="btn">Dificil</button>
+                    </div>
+                </div>
            </div>
        </div>
     )
